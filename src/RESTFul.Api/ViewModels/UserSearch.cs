f45a5f12e1917ea3cd84e0662ce3b5c2df91ba77
Bruno <bhdebrito@gmail.com>
2020-02-08 23:00:20 -0300

    update dep's

--- src/RESTFul.Api/ViewModels/UserSearch.cs
+++ src/RESTFul.Api/ViewModels/UserSearch.cs
@@ -6,16 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RESTFul.Api.ViewModels
 {
-    public class UserSearch : IRestSort, IRestPagination
+    public class UserSearch : IQuerySort, IQueryPaging
     {
 
         [FromQuery(Name = "lives_in")]
         public string Country { get; set; }
 
-        [FromQuery(Name = "older_than"), Rest(Operator = WhereOperator.GreaterThanOrEqualTo, HasName = "Age")]
+        [FromQuery(Name = "older_than"), QueryOperator(Operator = WhereOperator.GreaterThanOrEqualTo, HasName = "Age")]
         public int OlderThan { get; set; }
 
-        [FromQuery(Name = "younger_than"), Rest(Operator = WhereOperator.LessThanOrEqualTo, HasName = "Age")]
+        [FromQuery(Name = "younger_than"), QueryOperator(Operator = WhereOperator.LessThanOrEqualTo, HasName = "Age")]
         public int YoungerThan { get; set; }
 
         public int Offset { get; set; }
